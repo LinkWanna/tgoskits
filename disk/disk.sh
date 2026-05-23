bb9672758c1ed15179efe0c786edb7a4d09902ac
LinkWanna <lillluckenwp590@gmail.com>
2026-05-23 16:10:52 +0800

    fix: 修复了 `disk.sh` 中的 bug

--- disk/disk.sh
+++ disk/disk.sh
@@ -5,26 +5,25 @@ BUSY_BOX_DIR=busybox-1.37.0/_install
 set -e
 mkdir -p tmp
 
-taget=/dev/sda
+target=/dev/sda
 
 # 创建 MBR 分区表
-sudo parted -s ${taget} mklabel msdos
-sudo parted -s ${taget} mkpart primary fat32 1MiB 257MiB
-sudo parted -s ${taget} mkpart primary ext4 257MiB 4353MiB
-sudo parted -s ${taget} set 1 boot on561MiB
-sudo parted -s ${taget} set 1 boot on
+sudo parted -s ${target} mklabel msdos
+sudo parted -s ${target} mkpart primary fat32 1MiB 257MiB
+sudo parted -s ${target} mkpart primary ext4 257MiB 4353MiB
+sudo parted -s ${target} set 1 boot on
 
 # 格式化分区
-sudo mkfs.vfat -F32 ${taget}1 -n boot
-sudo mkfs.ext4 ${taget}2 -L rootfs
+sudo mkfs.vfat -F32 ${target}1 -n boot
+sudo mkfs.ext4 ${target}2 -L rootfs
 
 # 挂载 boot 分区并复制文件
-sudo mount ${taget}1 tmp
+sudo mount ${target}1 tmp
 sudo cp boot/* tmp/
 sudo umount tmp
 
 # 挂载 rootfs 分区并复制文件
-sudo mount ${taget}2 tmp
+sudo mount ${target}2 tmp
 sudo cp -a ${BUSY_BOX_DIR}/* tmp/
 sudo umount tmp
 
--- disk/fix.sh
+++ disk/fix.sh
@@ -5,13 +5,13 @@ BUSY_BOX_DIR=busybox-1.37.0/_install
 set -e
 mkdir -p tmp
 
-taget=/dev/sda
+target=/dev/sda
 
 # 因为我们经常直接断电，有时候 ext4 分区会坏掉，这里通过格式化来修复它
-sudo mkfs.ext4 ${taget}2 -L rootfs
+sudo mkfs.ext4 ${target}2 -L rootfs
 
 # 挂载 rootfs 分区并复制文件
-sudo mount ${taget}2 tmp
+sudo mount ${target}2 tmp
 sudo cp -a ${BUSY_BOX_DIR}/* tmp/
 sudo umount tmp
 
