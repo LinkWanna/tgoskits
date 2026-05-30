cc8f6ab0e91cffb5c2e500d02516090e4eae4346
LinkWanna <lillluckenwp590@gmail.com>
2026-05-30 17:47:42 +0800

    pipeline: 优化了构造内核的脚本

--- run.sh
+++ run.sh
@@ -25,11 +25,17 @@ elif [ "$CMD" = "build" ]; then
     cd uboot
     mkimage -f boot.its boot.sd
 
-    # 检查是否存在 /run/media/linkwanna/rootfs 目录
-    # 如果存在，则将 starry-sg2002.bin 和 ext4_100m.img 复制到该目录下
-    if [ -d "/run/media/linkwanna/boot" ]; then
+    # 检查是否存在 /dev/sda1
+    # 如果存在，则说明已经插入了 SD 卡，并且可以将生成的 boot.sd 复制到 SD 卡的 boot 分区中
+    # 如果存在，则将 starry-sg2002.bin 复制到该目录下
+    if [ -e "/dev/sda1" ]; then
+        echo "SD card detected. Copying boot.sd to SD card..."
+        sudo mkdir -p /run/media/linkwanna/boot
+        sudo mount /dev/sda1 /run/media/linkwanna/boot
         sudo cp boot.sd /run/media/linkwanna/boot
         sudo sync
+        sudo umount /run/media/linkwanna/boot
+        sudo rm -r /run/media/linkwanna/boot
     fi
 else
     echo "Usage: sh run.sh <command>"
